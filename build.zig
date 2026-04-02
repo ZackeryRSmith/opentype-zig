959520527aa25e4f51860287c8ca9f81c2c46884
Dumb Bird <zackery.smith82307@gmail.com>
2026-04-02 16:50:01 -0400

    chore: boilerplate

---
+++ build.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // const mod = b.addModule("opentype_zig", .{
+    // .root_source_file = b.path("src/root.zig"),
+    // .target = target,
+    // });
+
+    const exe = b.addExecutable(.{
+        .name = "opentype_zig",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{},
+        }),
+    });
+
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+}
